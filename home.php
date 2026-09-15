6825d4854370893666c18283691c6dd44cbad274
Real CT <nasteexacabdiraxmaan25@gmail.com>
2026-09-15 08:28:54 -0900

    Initial commit

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>PHP Practice</title> 
+</head> 
+<body> 
+
+    <?php 
+    // display echo or print 
+    echo "<h1>Hello World!</h1>"; 
+    echo "<h1>this is simple</h1>"; 
+    echo "<p>Nasteha</p>"; 
+
+    // print "ca233"."7"; 
+    echo ("nasteha " . "updy<br>"); 
+
+    $fullname = "nasta " . "ali"; 
+    echo "my name is $fullname <br>"; 
+
+    $my_str = 'welcome to php republic'; 
+    echo "Word count: " . str_word_count($my_str); 
+    ?> 
+
+</body> 
+</html>
